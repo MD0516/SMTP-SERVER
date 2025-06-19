68e3d0d0c06afbbf4e657065ad369b5965fe3953
Murali... <msmurali2005@gmail.com>
2025-06-19 08:20:55 +0530

    initial-commit

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { sendMailToUser, sendMailToCompany } from './Mailer.js';
+import cors from 'cors'
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use(cors({
+  origin: 'https://murali-portfolio-amber.vercel.app',
+  methods: ['POST'],
+  allowedHeaders: ['Content-Type']
+}));
+
+app.post('/api/contact', async (req, res) => {
+  const { name, email, message } = req.body;
+  try {
+    await sendMailToUser( name, email, message );
+    await sendMailToCompany( name, email, message );
+  } catch (err) {
+    console.error("Email error:", err);
+    res.status(500).send({ success: false, error: err.message });
+  }
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
